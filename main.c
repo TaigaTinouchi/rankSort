fe3ee19fd69ad30d61f581dec623e24147f7c525
TaigaTinouchi <tiger-0724h10@outlook.com>
2021-06-17 13:08:54 +0900

    add Docks

---
+++ main.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void){
+  int array[];
+  int N,M;
+  printf("Enter perfect score:");scanf("%d\n",&N);
+  printf("Enter worst score:");scanf("%d\n",&M);
+  printf("Enter test data as list:");scanf("%d\n", &array);
+  
+}
